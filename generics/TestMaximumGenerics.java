9caf733104b85a87f39a74c1600b2b225dc46961
gitlinux12 <mshirole94@gmail.com>
2022-09-14 23:42:44 +0530

    Find maximum integer at 3rd position using generics

---
+++ generics/TestMaximumGenerics.java
@@ -0,0 +1,37 @@
+package com.generics;
+
+public class TestMaximumGenerics {
+	public static Integer maxInteger( int x, int y, int z) {
+		if (x > y && x > z) {
+			return x;
+		} else if (y > z) {
+			return y;
+		} else {
+			return z;
+		}
+	}
+	public static void main(String[] args) {
+		System.out.println("Maximum among 3 No.= "+maxInteger(5,2,3));
+		
+		System.out.println("Test case 1.1");
+		// Test Case 1.1 maximum number at first position 
+		System.out.println("Maximum among 3 No : "+maxInteger(5, 2, 1));
+		System.out.println("Maximum among 3 No : "+maxInteger(6, 1, 2));
+		System.out.println("Maximum among 3 No : "+maxInteger(3, 1, 1));
+		
+		System.out.println("Test case 1.2");
+		// Test Case 1.2 maximum number at second position 
+		System.out.println("Maximum among 3 No : "+maxInteger(2, 4, 1));
+		System.out.println("Maximum among 3 No : "+maxInteger(1, 5, 2));
+		System.out.println("Maximum among 3 No : "+maxInteger(1, 6, 1));
+		
+		System.out.println("Test case 1.3");
+		// Test Case 1.1 maximum number at third position 
+		System.out.println("Maximum among 3 No : "+maxInteger(2, 1, 6));
+		System.out.println("Maximum among 3 No : "+maxInteger(1, 2, 7));
+		System.out.println("Maximum among 3 No : "+maxInteger(1, 1, 6));
+		
+		
+	}
+
+}
---
+++ generics/maximumInteger.java
@@ -0,0 +1,6 @@
+package com.generics;
+
+public class maximumInteger {
+	public static int maxOfInt
+
+}
